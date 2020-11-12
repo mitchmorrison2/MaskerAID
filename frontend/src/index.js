40749700f9f9d8b50baca3759d23ef8e7e8dfa87
ptarala99 <48962778+ptarala99@users.noreply.github.com>
2020-11-12 16:55:40 -0600

    homepage files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as distributor from './distributor';
-// This makes bootstrap work
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <React.StrictMode>
+  <React.StrictMode>
     <App />
-    </React.StrictMode>,
-     document.getElementById('root')
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-distributor.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
