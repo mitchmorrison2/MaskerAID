878dbce6bc37c9150343b2cddd8c0a361e83edaa
Mitch Morrison <47828551+mitchmorrison2@users.noreply.github.com>
2020-11-14 16:28:46 -0600

    Update server.js
    added cors

--- backend/node/server.js
+++ backend/node/server.js
@@ -35,6 +35,21 @@ app.use(bodyParser.json());
 app.use(cors({ 
   origin: '*'
 }));
+
+
+//CORS lite Setup
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
 //Attempting to connect to the database.
