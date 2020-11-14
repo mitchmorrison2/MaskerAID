0e328ad18f9b339c62d16f390fa54e4401544e72
Chelby Rhoades <chelbyjrhoades@gmail.com>
2020-11-14 15:16:12 -0600

    adding home stuff to pages

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,4 +1,6 @@
 import {CreateAccount} from "./pages/CreateAccount";
+import showHome from "./pages/Home";
+import { Itemdetails } from "./pages/itemdetails";
 import {Login} from "./pages/Login";
 import { OrderHistory } from "./pages/OrderHistory";
 import { UserProfile } from "./pages/UserProfile";
@@ -7,5 +9,6 @@ export const ROUTES = [
     { path: '/create', component: CreateAccount },
     { path: '/orders', component: OrderHistory},
     { path: '/userP', component: UserProfile},
+    { path: '/home', component: Itemdetails},
     { path: '/', component: Login}
 ]
